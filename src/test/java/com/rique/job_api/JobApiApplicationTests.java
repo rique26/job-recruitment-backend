0b1b36d6b69d66c1f3d41ea4154859056cee8a96
Pedro Henrique <henrique260600@gmail.com>
2026-08-10 09:34:47 -0300

    feat: feat: configuracao inicial do projeto job-api

---
+++ src/test/java/com/rique/job_api/JobApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rique.job_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
